f5d50bb9f198ab79351c3e9389ad9d0cb2bf1f0f
mualimkhair <mualimkhair@gmail.com>
2024-11-05 23:06:00 +0800

    layouting

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -8,11 +8,11 @@ const ItemList = ({ item = [] }) => {
         const { id, title, price, category, image } = item;
         return (
           <div className="grid gap-2">
-            <div className="bg-primary rounded-md w-44 h-60 flex flex-col justify-between">
-              <img src={image} alt="" className="h-1/2 w-full rounded-md" />
-              <div>
+            <div className="bg-primary rounded-md w-44 h-60 flex flex-col justify-between relative">
+              <img src={image} alt="" className="h-60 w-full rounded-md" />
+              <div className="absolute bottom-0 bg-primary w-full rounded-md">
                 <h3>{title}</h3>
-                <p>{price}</p>
+                <p className="bg-secondary text-primary text-center">${price}</p>
               </div>
             </div>
             <button className="hover:bg-secondary bg-primary hover:text-primary text-secondary transition ease-in-out duration-300 w-full flex justify-center rounded-md text-lg ">
